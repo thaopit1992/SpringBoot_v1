fe76e2bdb71df0a4c852a0e79bfa8c63fe83a991
thaopx <thaopx@vnpt.vn>
2023-06-09 15:36:05 +0700

    API hello world

---
+++ src/main/java/com/thaopx/RetailStore/RetailStoreApplication.java
@@ -0,0 +1,13 @@
+package com.thaopx.RetailStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreApplication.class, args);
+	}
+
+}
